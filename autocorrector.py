144623df0c4cfedc8dc160eef7a932be6025e46e
Anderson Andrade <anderson_andrade_@outlook.com>
2024-02-05 18:17:26 -0300

    fix: implement to write words with accents increment more words in  dictionary

--- autocorrector.py
+++ autocorrector.py
@@ -1,7 +1,7 @@
 import keyboard
 import pyautogui
-import time
-# from words import *
+from pynput.keyboard import Controller
+from words import *
 
 ignored_keys = ['ctrl', 'alt', 'shift', 'tab', 'centerentedowndowny/', 'up', 'down', 'left', 'right']
 typed_string = ""
@@ -10,11 +10,6 @@ def replace_words(s):
     for word, replacement in word_replacements.items():
         s = s.replace(word, replacement)
     return s
-
-word_replacements = {
-    'nao':'não ',
-    'say':'sai '
-}
 
 def check_words(s):
   return s in word_replacements
@@ -26,8 +21,7 @@ def on_key_event(e):
         if e.name == 'space' and check_words(typed_string):
             text = replace_words(typed_string) + ' '
             pyautogui.hotkey('ctrl', 'backspace')
-            pyautogui.write(text, _pause=False, _keyboardLayout=pyautogui.KEYBOARD_KEYS['portuguese'])
-            print(text)
+            Controller().type(text)
             typed_string = ''
         elif e.name == 'backspace':
             typed_string = typed_string[:-1]  # Remove the last 
@@ -36,8 +30,6 @@ def on_key_event(e):
             print('here')
         elif e.name not in ignored_keys:
             typed_string += e.name
-            
-        print(f'Typed String: {typed_string}')
 
 keyboard.hook(on_key_event)
 keyboard.wait("esc")
